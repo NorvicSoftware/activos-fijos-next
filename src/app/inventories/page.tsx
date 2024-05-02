390c2a6437ca6c6aa335765ccd2a526a31060a59
vincent292 <ariasvincent292@gmail.com>
2024-05-02 17:12:49 -0400

    Run

--- src/app/inventories/page.tsx
+++ src/app/inventories/page.tsx
@@ -1,10 +1,51 @@
+"use client"
+import axios from "axios";
 import LinkButton from "../components/linkButton";
+import { useState, useEffect } from "react";
+
 export default function Inventories() {
+    const [inventories, setinventories] = useState<any>([]);
+
+    axios.get('http://127.0.0.1:8000/api/inventories')
+    .then(function(response){
+        setinventories(response.data.inventories);
+    })
+    .catch(function (error) {
+        console.log('error' + error);
+    });
+
+
     return (
-        <>
-        <h2>INVENTARIO DE ACTIVOS FIJOS</h2>
-        <LinkButton name="Activos fijos" route="/assets" />
-        </>
-        
-    )
+        <div>
+            <h2 className="flex justify-center">LISTADO DE INVENTARIOS</h2>
+           
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Fecha de inicio</th>
+                        <th>Fecha final</th>
+                        <th>Detalles</th>
+                        <th>No Leído</th>
+                        <th>Leído</th>
+                    </tr>
+                </thead>
+                <tbody>
+    {inventories.map((inventory:any, id:number) => (
+        <tr key={inventory.id}>
+            <td>{inventory.name}</td>
+            <td>{inventory.start_Date}</td>
+            <td>{inventory.final_date}</td>
+            <td>{inventory.details}</td>
+            <td>{inventory.not_read}</td>
+            <td>{inventory.read}</td>
+            <LinkButton name="ver detalles" route="/inventories/read" />
+            
+        </tr>
+    ))}
+</tbody>
+
+            </table>
+        </div>
+    );
 }
---
+++ src/app/inventories/read/page.tsx
@@ -0,0 +1,47 @@
+"use client"
+import LinkButton from "@/app/components/linkButton";
+import axios from "axios";
+import { useState } from "react";
+
+export default function read() {
+
+    const title: string = "LISTADO DE ACTIVOS FIJOS inventariados";
+    const [assets, setAssets] = useState<any>([]);
+
+    axios.get('http://127.0.0.1:8000/api/inventories/read/assets')
+    .then(function(response){
+        setAssets(response.data.assets);
+    })
+    .catch(function (error) {
+        console.log('error' + error);
+    });
+
+    return (
+        <div>
+            <h2 className="flex justify-center">{title}</h2>
+            <LinkButton name="Crear nuevo activo fijo" route="/assets/create" />
+            <table>
+                <thead>
+                    <tr>
+                        <th>Activo fijo</th>
+                        <th>Codigo</th>
+                        <th>Descripcion</th>
+                        <th>Marca</th>
+                        <th>Modelo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { assets.map((asset:any, id:number) =>(
+                        <tr key={id}>
+                            <td>{asset.name}</td>
+                            <td>{asset.code}</td>
+                            <td>{asset.description}</td>
+                            <td>{asset.brand}</td>
+                            <td>{asset.model}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
